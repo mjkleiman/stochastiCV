963e41c58cab7f1510c8c79d6183c352bfe1195f
mjkleiman <mjkleiman@gmail.com>
2021-06-03 16:56:39 -0400

    0.1.2

---
+++ README.md
@@ -0,0 +1,6 @@
+StochastiCV
+==============================
+
+A method of cross-validation based on scikit-learn that splits data into train/valid/test splits one or more times (using random or assigned seed values) and then repeats the model multiple times using different seeds. This function enables a more statistical and scientific method of investigating model performance.
+
+
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='stochastiCV',
+    packages=find_packages(
+        where = 'stochastiCV'
+    ),
+    version='0.1.1',
+    description='A method of cross-validation based on scikit-learn that splits data into train/valid/test splits two or more times (using random or assigned seed values) and then repeats the model multiple times using different seeds. This function enables a more statistical and scientific method of investigating model performance.',
+    author='Michael J Kleiman',
+    author_email='michael@kleiman.me',
+    license='BSD-3',
+)
